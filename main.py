12ecb4e5c0918dcaf54be4f57864df26d6d571b3
alonsocontr <130825228+alonsocontr@users.noreply.github.com>
2024-12-21 13:54:56 -0800

    main.py

--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 # 2. Create a GUI using PYQt5 where users can add/remove a stock
 #    from their watchlist
 #       - Users add stock by searching for a ticker
-#       - Users remove stock by selecting the stock in their watchlist
+#       - Users remove stock by typing the ticker in their watchlist
 #         then press remove button
 # 3. Add a stock details button that adds details about a stock on a
 #    user's watchlist. This includes graphs, historical data, public sentiment
 #    and analyst ratings/average stock forecasts. Need more APIs here...
-# 4. Add user authentication.
-# 5. Add a notifications system either through email or SMS
+# 4. Add a notifications system either through email or SMS
 import interface
 
 
